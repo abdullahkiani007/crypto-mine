7b765241b52fa8011b411dffce924cc5824b6415
Abdul Wahab <abdul.wahab@codistan.org>
2023-07-12 14:52:22 +0500

    internal.js

--- frontend/src/api/internal.js
+++ frontend/src/api/internal.js
@@ -21,9 +21,10 @@ export const login = async (data) => {
 
 export const signUp = async (data) => {
   let response;
-  console.log("signup", response);
+
   try {
     response = await api.post("/register", data);
+    console.log("signup", response);
   } catch (error) {
     return error;
   }
